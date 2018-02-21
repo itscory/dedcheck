ea484619169b5f7884b21610d97e28d2c1b688a7
Corey Stewart <itscory@users.noreply.github.com>
2018-02-21 16:57:04 -0500

    script will echo date of first exim_mainlog entry if server is spamming

--- dedcheck.sh
+++ dedcheck.sh
@@ -94,7 +94,7 @@ echo -e "${color}Queue: $(exim -bpc)${NC}"
 echo -ne "\n"
 # Display top dovecot senders if server is spamming.
 if [ "$isspamming" == "1" ]
-then echo "------Top dovecot authenticated emails------"
+then echo "------Dovecot emails sent since $(head -1 /var/log/exim_mainlog |awk '{print $1}')------"
 grep "A=dovecot_login" /var/log/exim_mainlog | awk -F"A=dovecot_login:" {'print $2'} | cut -f1 -d' ' | sort | uniq -c | sort -n | awk {'print $1, " unique emails sent by " , $2'}
 echo -ne "\n"
 fi
